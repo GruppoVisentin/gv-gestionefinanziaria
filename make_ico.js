83c0297d940d056c284cec2f006a1a04df3844ad
GruppoVisentin <gruppovisentinsrl@gmail.com>
2026-06-09 15:03:51 +0200

    feat: add robust true favicon.ico for Windows PWA desktop shortcut generation

---
+++ make_ico.js
@@ -0,0 +1,9 @@
+import pngToIco from 'png-to-ico';
+import fs from 'fs';
+
+pngToIco('public/icon-512-v7.png')
+  .then(buf => {
+    fs.writeFileSync('public/favicon.ico', buf);
+    console.log('favicon.ico created successfully');
+  })
+  .catch(console.error);
